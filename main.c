bc692d9c22dc90f37bc21c33183fb018d3bc1d92
520Matches <HengDi_Sun@163.com>
2025-02-14 14:07:02 +0800

    fix usb

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
+#include "core_cm3.h"
+
+#include "usb_type.h"
+#include "usb_core.h"
+#include "usb_pwr.h"
+#include "usb_init.h"
+
+#include "hw_config.h"
+
+__IO uint8_t PrevXferComplete = 1;
 
 int main(void)
 {
-    while(1);
+	Set_System();
+	USB_Interrupts_Config();
+	Set_USBClock();
+	USB_Init();
+
+	while(1)
+	{
+		if(bDeviceState == CONFIGURED)
+		{
+			if(PrevXferComplete)
+			{
+				// RHIDCheckState();
+			}
+		}
+	}
     return 0;
 }
